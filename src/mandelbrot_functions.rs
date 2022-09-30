38b8dbd4445dc1dd726a2b8a031b9bf2bd7b31eb
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-30 22:42:30 +0100

    Bugfix

--- src/mandelbrot_functions.rs
+++ src/mandelbrot_functions.rs
@@ -11,9 +11,9 @@ fn calculateRatio(w: u32, h: u32) -> f64 {
     let wf: f64 = w.into();
     let hf: f64 = h.into();
     if wf > hf * 1.5 {
-        ratio = 2 / hf;
+        ratio = 2.0 / hf;
     } else {
-        ratio = 3 / wf;
+        ratio = 3.0 / wf;
     }
     ratio
 }
