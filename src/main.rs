91ae9a1f8d68a713d4bbc308f7aba80a64911250
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-30 22:41:36 +0100

    prototyped mandelbrot_functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod mandelbrot_functions;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/mandelbrot_functions.rs
@@ -0,0 +1,25 @@
+use num::complex::Complex;
+
+pub fn posPartOfSet() -> bool {}
+
+fn compPartOfSet() -> bool {}
+
+fn screenToComplex(w: u32, h: u32, x: u32, y: u32) -> Complex<f64> {}
+
+fn calculateRatio(w: u32, h: u32) -> f64 {
+    let ratio: f64;
+    let wf: f64 = w.into();
+    let hf: f64 = h.into();
+    if wf > hf * 1.5 {
+        ratio = 2 / hf;
+    } else {
+        ratio = 3 / wf;
+    }
+    ratio
+}
+
+#[cfg(test)]
+mod mandelbrot_functions_test {
+    #[test]
+    fn posPartOfSet_test() {}
+}
