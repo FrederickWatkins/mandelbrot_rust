05f0a23d8b376ec5ed8b33b4485b1a7c7802a236
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-30 23:19:19 +0100

    Did a bunch of crap

--- src/mandelbrot_functions.rs
+++ src/mandelbrot_functions.rs
@@ -1,12 +1,27 @@
-use num::complex::Complex;
+use num::Complex;
 
-pub fn posPartOfSet() -> bool {}
+pub fn pos_part_of_set(w:u32, h:u32, x:u32, y:u32, iterations: u32) -> bool {
+    comp_part_of_set(screen_to_complex(w, h, x, y), iterations)
+}
 
-fn compPartOfSet() -> bool {}
+fn comp_part_of_set(c: Complex<f64>, iterations: u32) -> bool {
+    let z = Complex::<f64>::new(0.0, 0.0);
+    for i in 0..iterations {
+        z = (z * z) + c;
+        if z.norm() > 6000000 {
+            false
+        }
+    }
+}
 
-fn screenToComplex(w: u32, h: u32, x: u32, y: u32) -> Complex<f64> {}
+fn screen_to_complex(w: u32, h: u32, x: u32, y: u32) -> Complex<f64> {
+    let ratio = calculate_ratio(w, h);
+    let r = (x.into() - (w.into() / 1.5)) * ratio;
+    let i = ((h.into() / 2) - y.into()) * ratio;
+    Complex::<f64>::new(r, i)
+}
 
-fn calculateRatio(w: u32, h: u32) -> f64 {
+fn calculate_ratio(w: u32, h: u32) -> f64 {
     let ratio: f64;
     let wf: f64 = w.into();
     let hf: f64 = h.into();
@@ -21,5 +36,6 @@ fn calculateRatio(w: u32, h: u32) -> f64 {
 #[cfg(test)]
 mod mandelbrot_functions_test {
     #[test]
-    fn posPartOfSet_test() {}
+    fn pos_part_of_set_test1() {
+    }
 }
