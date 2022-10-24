84b624b347eca6f1ae11574db22c6c0ea7ba1276
Frederick Watkins <freddiewatkins6@gmail.com>
2022-10-24 21:48:37 +0100

    Created struct to store runtime config

--- src/mandelbrot_functions.rs
+++ src/mandelbrot_functions.rs
@@ -1,40 +1,48 @@
 use num::Complex;
 
-pub fn pos_part_of_set(w: u32, h: u32, x: u32, y: u32, iterations: u32) -> bool {
-    comp_part_of_set(screen_to_complex(w, h, x, y), iterations)
+struct MandelbrotCalculator {
+    width: f64,
+    height: f64,
+    ratio: f64,
+    iterations: u32,
 }
 
-fn comp_part_of_set(c: Complex<f64>, iterations: u32) -> bool {
-    let mut z = Complex::<f64>::new(0.0, 0.0);
-    for _i in 0..iterations {
-        z = (z * z) + c;
-        if z.norm() > 6000000.0 {
-            return false;
+impl MandelbrotCalculator {
+    pub fn pos_part_of_set(self, x: u32, y: u32) -> bool {
+        self.comp_part_of_set(self.screen_to_complex(x, y))
+    }
+    
+    pub fn comp_part_of_set(self, c: Complex<f64>) -> bool {
+        let mut z = Complex::<f64>::new(0.0, 0.0);
+        for _i in 0..self.iterations {
+            z = (z * z) + c;
+            if z.norm() > 6000000.0 {
+                return false;
+            }
+        }
+        z.norm() < 1000.0
+    }
+    
+    fn screen_to_complex(self, x: u32, y: u32) -> Complex<f64> {
+        let xf: f64 = x.into();
+        let yf: f64 = y.into();
+    
+        let r = (xf - (self.width / 1.5)) * self.ratio;
+        let i = ((self.height / 2.0) - yf) * self.ratio;
+        Complex::<f64>::new(r, i)
+    }
+    
+    fn change_config(mut self, width: u32, height: u32) {
+        self.width = width.into();
+        self.height = height.into();
+        if self.width > self.height * 1.5 {
+            self.ratio = 2.0 / self.height;
+        } else {
+            self.ratio = 3.0 / self.width;
         }
     }
-    z.norm() < 1000.0
 }
 
-fn screen_to_complex(w: u32, h: u32, x: u32, y: u32) -> Complex<f64> {
-    let wf: f64 = w.into();
-    let hf: f64 = h.into();
-    let xf: f64 = x.into();
-    let yf: f64 = y.into();
-
-    let ratio = calculate_ratio(wf, hf);
-
-    let r = (xf - (wf / 1.5)) * ratio;
-    let i = ((hf / 2.0) - yf) * ratio;
-    Complex::<f64>::new(r, i)
-}
-
-fn calculate_ratio(w: f64, h: f64) -> f64 {
-    if w > h * 1.5 {
-        2.0 / h
-    } else {
-        3.0 / w
-    }
-}
 
 
 #[cfg(test)]
@@ -58,6 +66,9 @@ mod mandelbrot_functions_test {
 
     #[test]
     fn screen_to_complex_test_2() {
-        assert_eq!(screen_to_complex(1920, 1080, 1920, 1080), Complex::<f64>::new(16.0 / 9.0, -1.0))
+        assert_eq!(
+            screen_to_complex(1920, 1080, 1920, 1080),
+            Complex::<f64>::new(16.0 / 9.0, -1.0)
+        )
     }
 }
